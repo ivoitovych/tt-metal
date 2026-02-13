e2565f5707e7ed3d868b17956daafe1fdfbec48c
Iaroslav Voitovych <yaroslav.voytovych@gmail.com>
2026-02-13 09:23:27 -0700

    Verify: remove ADJUST_COMPILER_WARNINGS() to prove it is dead code

--- tt-train/cmake/compilers.cmake
+++ tt-train/cmake/compilers.cmake
@@ -84,36 +84,5 @@ function(CHECK_COMPILERS)
     endif()
 endfunction()
 
-function(ADJUST_COMPILER_WARNINGS)
-    if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        target_compile_options(
-            compiler_warnings
-            INTERFACE
-                -Wsometimes-uninitialized
-                -Wno-c++11-narrowing
-                -Wno-error=local-type-template-args
-                -Wno-delete-non-abstract-non-virtual-dtor
-                -Wno-c99-designator
-                -Wno-shift-op-parentheses
-                -Wno-non-c-typedef-for-linkage
-                -Wno-deprecated-this-capture
-                -Wno-deprecated-volatile
-                -Wno-deprecated-builtins
-                -Wno-deprecated-declarations
-        )
-    else() # GCC-12 or higher
-        target_compile_options(
-            compiler_warnings
-            INTERFACE
-                -Wno-deprecated
-                -Wno-attributes
-                -Wno-stringop-overread
-                -Wno-stringop-overflow
-                -Wno-maybe-uninitialized
-                -Wno-missing-requires
-                -Wno-narrowing
-                -Wno-non-template-friend
-                -Wno-error=non-template-friend
-        )
-    endif()
-endfunction()
+# ADJUST_COMPILER_WARNINGS() removed to verify it is dead code.
+# The function was never called and the compiler_warnings target does not exist.
